59860251782854786d9d86667b28f93c87c40e6d
ffrederick <45361225+ffrederick@users.noreply.github.com>
2018-11-26 21:01:43 +0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+int n, k, a[1001], du[1001], f[1001], cs[1001];
+int j = 0;
+void truyvet(int i, int &j)
+{
+    if (i <= n)
+    {
+        du[i] = (du[i-1] + a[i]) % k;
+        if (cs[du[i]] != -1)
+            f[i] = max(f[i-1], f[cs[du[i]]] + i - cs[du[i]]);
+        else
+        {
+            cs[du[i]] = i;
+            f[i] = f[i-1];
+        }
+        truyvet(i+1, j);
+        if (i > 0)
+        {
+            if (i != j)
+            {
+                cout << a[i] << " ";
+            }
+            else
+            {
+                j = cs[du[i-1]];
+            }
+        }
+    }
+    else j = cs[du[n]];
+}
+int main()
+{
+    cin >> n >> k;
+    du[0] = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> a[i];
+        cs[i] = -1;
+        f[i] = 0;
+    }
+    truyvet(1, j);
+    return 0;
+}
